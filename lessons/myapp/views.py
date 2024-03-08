d5700f48426667cbafa0c67289d8b08961c497d5
Gaurund <pavel.makushev@gmail.com>
2024-03-08 16:03:29 +0300

    Homework 1

--- lessons/myapp/views.py
+++ lessons/myapp/views.py
@@ -2,49 +2,18 @@ import logging
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 logger = logging.getLogger(__name__)
 
 
 def index(response):
-    page = """
-    <!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Главная</title>
-</head>
-<body>
-    <div id="menu">
-        <div class="menu-item"><a href="#">Главная</a></div>
-        <div class="menu-item"><a href="about/">О нас</a></div>
-    </div>
-    <h1>Главная</h1>
-    <p>Главная страница сайта.</p>
-</body>
-</html>"""
+    template = loader.get_template("index.html")
     logger.info('Index page accessed')
-    return HttpResponse(page)
+    return HttpResponse(template.render())
 
 
 def about(request):
-    page = """
-        <!DOCTYPE html>
-    <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>О нас</title>
-    </head>
-    <body>
-        <div id="menu">
-        <div class="menu-item"><a href="/">Главная</a></div>
-        <div class="menu-item"><a href="#">О нас</a></div>
-    </div>
-        <h1>О нас</h1>
-        <p>Страница описания "О нас". Чего-то тут должно быть написано.</p>
-    </body>
-    </html>"""
+    template = loader.get_template("about.html")
     logger.info('About page accessed')
-    return HttpResponse(page)
+    return HttpResponse(template.render())
