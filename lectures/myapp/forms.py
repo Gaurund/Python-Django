50c9b374423c826caa0ac7a5d279d40e68df356c
Gaurund <pavel.makushev@gmail.com>
2024-03-15 14:04:53 +0300

    Lecture 04 5

--- lectures/myapp/forms.py
+++ lectures/myapp/forms.py
@@ -8,6 +8,19 @@ class UserForm(forms.Form):
     email = forms.EmailField()
     age = forms.IntegerField(min_value=0, max_value=120)
 
+    def clean_name(self):
+        """Плохой пример. Подмена параметра min_length."""
+        name = self.cleaned_data['name']
+        if len(name) < 3:
+            raise forms.ValidationError('Имя должно содержать не менее 3 символов')
+        return name
+
+    def clean_email(self):
+        email: str = self.cleaned_data['email']
+        if not (email.endswith('vk.team') or email.endswith('corp.mail.ru')):
+            raise forms.ValidationError('Используйте корпоративную почту')
+        return email
+
 
 class ManyFieldsForm(forms.Form):
     name = forms.CharField(max_length=50)
@@ -30,7 +43,7 @@ class ManyFieldsFormWidget(forms.Form):
     height = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
     birthdate = forms.DateField(initial=datetime.date.today,
-                                widget=forms.DateInput(attrs={'class': 'form-control', 'type':'date'}))
+                                widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
     gender = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')],
                                widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
