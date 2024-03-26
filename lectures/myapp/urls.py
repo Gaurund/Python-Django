3b9fef04848b67fe58604f925f9cab8149c4911e
Gaurund <pavel.makushev@gmail.com>
2024-03-26 10:58:04 +0300

    Lecture 6 - 2

--- lectures/myapp/urls.py
+++ lectures/myapp/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('forms/', views.many_fields_form, name='many_fields_form'),
     path('user/', views.add_user, name="add_user"),
     path('upload/', views.upload_image, name='upload_image'),
-
+    path('db/', views.total_in_db, name='db'),
+    path('view/', views.total_in_view, name='view'),
+    path('template/', views.total_in_template, name='template'),
 ]
